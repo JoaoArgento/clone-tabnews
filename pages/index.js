37182b3c407b7f331e6d28bc3f596fac5ff26edf
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2025-11-24 23:11:57 +0000

    adicionado a extensão prettier

--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,179 @@
 function Home() {
-    return (
-        <div>
-            <h1>Rap do Ademir</h1>
+  return (
+    <div>
+      <h1>Rap do Ademir</h1>
 
-            <p>
-                <span>Esse som é dedicado pra empreendedores</span><br />
-                <span>E empreendedoras que percorrem um</span><br />
-                <span>Caminho solitário</span><br /><br />
+      <p>
+        <span>Esse som é dedicado pra empreendedores</span>
+        <br />
+        <span>E empreendedoras que percorrem um</span>
+        <br />
+        <span>Caminho solitário</span>
+        <br />
+        <br />
 
-                <span>Para todos os que buscam se</span><br />
-                <span>Tornarem mais fortes na luta</span><br />
-                <span>Contra os seus adversários</span><br /><br />
+        <span>Para todos os que buscam se</span>
+        <br />
+        <span>Tornarem mais fortes na luta</span>
+        <br />
+        <span>Contra os seus adversários</span>
+        <br />
+        <br />
 
-                <span>Seja medo ou fraqueza ou então</span><br />
-                <span>Qualquer barreira, encontre a força</span><br />
-                <span>No orgulho</span><br /><br />
+        <span>Seja medo ou fraqueza ou então</span>
+        <br />
+        <span>Qualquer barreira, encontre a força</span>
+        <br />
+        <span>No orgulho</span>
+        <br />
+        <br />
 
-                <span>Player João mais uma vez</span><br />
-                <span>Trazendo para vocês</span><br /><br />
+        <span>Player João mais uma vez</span>
+        <br />
+        <span>Trazendo para vocês</span>
+        <br />
+        <br />
 
-                <span>A essência de Ademir</span><br />
-                <span>Inspiração de uma vida!</span><br /><br />
+        <span>A essência de Ademir</span>
+        <br />
+        <span>Inspiração de uma vida!</span>
+        <br />
+        <br />
 
-                <span>Eu tenho orgulho pelo sangue que corre nas minhas veias</span><br />
-                <span>Orgulho por eu ser elite em classe empreendedora</span><br />
-                <span>Orgulho por vencer as tragédias do meu passado</span><br />
-                <span>E de saber seguir em frente mesmo sem ninguém do lado</span><br /><br />
+        <span>Eu tenho orgulho pelo sangue que corre nas minhas veias</span>
+        <br />
+        <span>Orgulho por eu ser elite em classe empreendedora</span>
+        <br />
+        <span>Orgulho por vencer as tragédias do meu passado</span>
+        <br />
+        <span>E de saber seguir em frente mesmo sem ninguém do lado</span>
+        <br />
+        <br />
 
-                <span>Orgulho, eu tenho, e isso ninguém tira de mim</span><br />
-                <span>Minha marra te incomoda? Lamento, eu sou assim</span><br /><br />
+        <span>Orgulho, eu tenho, e isso ninguém tira de mim</span>
+        <br />
+        <span>Minha marra te incomoda? Lamento, eu sou assim</span>
+        <br />
+        <br />
 
-                <span>Menosprezo os que são fracos, pois não se comparam a mim</span><br />
-                <span>E se você me pergunta se eu empreendo muito, eu te respondo sim</span><br /><br />
+        <span>Menosprezo os que são fracos, pois não se comparam a mim</span>
+        <br />
+        <span>
+          E se você me pergunta se eu empreendo muito, eu te respondo sim
+        </span>
+        <br />
+        <br />
 
-                <span>Intenso empreendimento</span><br />
-                <span>Mas eu suporto além, por isso aumento o peso</span><br />
-                <span>Eu pago o preço, de onde eu vim eu sei nunca me esqueço</span><br />
-                <span>E de toda evolução que eu tive desde o começo</span><br /><br />
+        <span>Intenso empreendimento</span>
+        <br />
+        <span>Mas eu suporto além, por isso aumento o peso</span>
+        <br />
+        <span>Eu pago o preço, de onde eu vim eu sei nunca me esqueço</span>
+        <br />
+        <span>E de toda evolução que eu tive desde o começo</span>
+        <br />
+        <br />
 
-                <span>Alguns falam: Você mudou</span><br />
-                <span>Outros falam: Você falhou</span><br />
-                <span>Eles pensam que me importo</span><br />
-                <span>Não sabem quem é que sou</span><br /><br />
+        <span>Alguns falam: Você mudou</span>
+        <br />
+        <span>Outros falam: Você falhou</span>
+        <br />
+        <span>Eles pensam que me importo</span>
+        <br />
+        <span>Não sabem quem é que sou</span>
+        <br />
+        <br />
 
-                <span>Mas minha essência nunca mudou</span><br />
-                <span>Fecho a mão, bato no peito, orgulho de ser quem sou</span><br /><br />
+        <span>Mas minha essência nunca mudou</span>
+        <br />
+        <span>Fecho a mão, bato no peito, orgulho de ser quem sou</span>
+        <br />
+        <br />
 
-                <span>Meu coração é puro</span><br />
-                <span>Meu coração é puro (pura maldade sim)</span><br />
-                <span>Tão grande quanto minha força é o orgulho que há em mim</span><br />
-                <span>Um Empreendedor orgulhoso, eu sou assim</span><br />
-                <span>Meu coração é puro</span><br />
-                <span>Meu coração é puro (pura maldade sim)</span><br />
-                <span>Tão grande quanto minha força é o orgulho que há em mim</span><br />
-                <span>Um Empreendedor orgulhoso, eu sou assim</span><br /><br />
+        <span>Meu coração é puro</span>
+        <br />
+        <span>Meu coração é puro (pura maldade sim)</span>
+        <br />
+        <span>Tão grande quanto minha força é o orgulho que há em mim</span>
+        <br />
+        <span>Um Empreendedor orgulhoso, eu sou assim</span>
+        <br />
+        <span>Meu coração é puro</span>
+        <br />
+        <span>Meu coração é puro (pura maldade sim)</span>
+        <br />
+        <span>Tão grande quanto minha força é o orgulho que há em mim</span>
+        <br />
+        <span>Um Empreendedor orgulhoso, eu sou assim</span>
+        <br />
+        <br />
 
-                <span>Então entenda bem (Então entenda bem!)</span><br />
-                <span>Não tenho esposa e amigos, mas ouça, eu não mudei</span><br />
-                <span>Apenas evolui, eu sei que me superei</span><br />
-                <span>Você percebe só em ver o nível que eu cheguei</span><br /><br />
+        <span>Então entenda bem (Então entenda bem!)</span>
+        <br />
+        <span>Não tenho esposa e amigos, mas ouça, eu não mudei</span>
+        <br />
+        <span>Apenas evolui, eu sei que me superei</span>
+        <br />
+        <span>Você percebe só em ver o nível que eu cheguei</span>
+        <br />
+        <br />
 
-                <span>Ei, mais forte do que nunca!</span><br />
-                <span>Se não viu, me veja anos atrás</span><br />
-                <span>Hoje tudo evoluiu</span><br /><br />
+        <span>Ei, mais forte do que nunca!</span>
+        <br />
+        <span>Se não viu, me veja anos atrás</span>
+        <br />
+        <span>Hoje tudo evoluiu</span>
+        <br />
+        <br />
 
-                <span>Ainda sou frio, calculista e nem um pouco gentil</span><br />
-                <span>Mas supero Empreitadas acima de 8 MIL, viu?</span><br /><br />
+        <span>Ainda sou frio, calculista e nem um pouco gentil</span>
+        <br />
+        <span>Mas supero Empreitadas acima de 8 MIL, viu?</span>
+        <br />
+        <br />
 
-                <span>Deus, tenho em mim a força de um Deus</span><br />
-                <span>Há algo que me ilumina, se ainda não percebeu</span><br /><br />
+        <span>Deus, tenho em mim a força de um Deus</span>
+        <br />
+        <span>Há algo que me ilumina, se ainda não percebeu</span>
+        <br />
+        <br />
 
-                <span>Eu sei que eu não me importo com nada que não é meu</span><br />
-                <span>Os empreendedores, mas nem tão grandes quanto eu</span><br /><br />
+        <span>Eu sei que eu não me importo com nada que não é meu</span>
+        <br />
+        <span>Os empreendedores, mas nem tão grandes quanto eu</span>
+        <br />
+        <br />
 
-                <span>Meu coração é puro</span><br />
-                <span>Meu coração é puro (pura maldade sim)</span><br />
-                <span>Tão grande quanto minha força e o orgulho que há em mim</span><br />
-                <span>Um Empreendedor orgulhoso, eu sou assim</span><br />
-                <span>Não me conformo com fraqueza por isso me torno forte</span><br />
-                <span>Eu sigo nesse caminho até o dia da minha morte</span><br />
-                <span>Não há sorte, é treino, esforço até o fim</span><br />
-                <span>Por isso que eu entendo minha esposa fugir de mim</span><br /><br />
+        <span>Meu coração é puro</span>
+        <br />
+        <span>Meu coração é puro (pura maldade sim)</span>
+        <br />
+        <span>Tão grande quanto minha força e o orgulho que há em mim</span>
+        <br />
+        <span>Um Empreendedor orgulhoso, eu sou assim</span>
+        <br />
+        <span>Não me conformo com fraqueza por isso me torno forte</span>
+        <br />
+        <span>Eu sigo nesse caminho até o dia da minha morte</span>
+        <br />
+        <span>Não há sorte, é treino, esforço até o fim</span>
+        <br />
+        <span>Por isso que eu entendo minha esposa fugir de mim</span>
+        <br />
+        <br />
 
-                <span>Yeah, Player João</span><br />
-                <span>Mais uma vez</span><br /><br />
+        <span>Yeah, Player João</span>
+        <br />
+        <span>Mais uma vez</span>
+        <br />
+        <br />
 
-                <span>Eu vou continuar e nada pode me para</span><br />
-                <span>Por que você são minha força tamo junto vamo lá</span>
-            </p>
-        </div>
-    );
+        <span>Eu vou continuar e nada pode me para</span>
+        <br />
+        <span>Por que você são minha força tamo junto vamo lá</span>
+      </p>
+    </div>
+  );
 }
-
 
 export default Home;
