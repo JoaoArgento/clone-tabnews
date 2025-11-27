1c86c1f97eb5cbddc2e2a1fc6d36b688233c0dbc
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2025-11-27 13:50:18 +0000

    adicionado o .env e algumas variaveis de ambiente para o funcionamento do postgres

---
+++ infra/database.js
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+
+async function Query(queryInfo)
+{
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  console.log(client);
+  
+  await client.connect();
+  const result = await client.query(queryInfo);
+  await client.end();
+
+  return result;
+}
+
+
+export default {
+  query: Query
+}
