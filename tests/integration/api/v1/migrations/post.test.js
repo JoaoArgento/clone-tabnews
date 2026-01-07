b81eb32d2545c84ade0a8e6ed1ef0c1786e03e43
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2026-01-07 20:45:31 +0000

    test: update all descriptions to adapt to a new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,22 +6,31 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST in api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST in api/v1/migrations", () => {
+  describe("Anonymous User", () => {
+    describe("Retrieving pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const migrations = await response.json();
+
+        expect(Array.isArray(migrations)).toBe(true);
+        expect(migrations.length).toBeGreaterThan(0);
+
+        const migrationsCountRow = await database.query(
+          "SELECT count(*)::int FROM pgmigrations;",
+        );
+
+        const migrationsCount = migrationsCountRow.rows[0].count;
+
+        expect(migrationsCount).toBe(migrations.length);
+      });
+    });
   });
-  expect(response.status).toBe(201);
-
-  const migrations = await response.json();
-
-  expect(Array.isArray(migrations)).toBe(true);
-  expect(migrations.length).toBeGreaterThan(0);
-
-  const migrationsCountRow = await database.query(
-    "SELECT count(*)::int FROM pgmigrations;",
-  );
-
-  const migrationsCount = migrationsCountRow.rows[0].count;
-
-  expect(migrationsCount).toBe(migrations.length);
 });
