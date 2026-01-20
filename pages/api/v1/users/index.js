b61f51f2739648e3a4bb5ef0bdf91a4890f77783
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2026-01-20 13:22:56 +0000

    fix: runs migrations in user endpoint

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller";
 import user from "models/user.js";
+import orchestrator from "tests/orchestrator";
 const router = createRouter();
 
 router.post(postHandler);
@@ -8,6 +9,8 @@ router.post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function postHandler(request, response) {
+  await orchestrator.runPendingMigrations();
+
   const userInput = request.body;
   const newUser = await user.create(userInput);
 
