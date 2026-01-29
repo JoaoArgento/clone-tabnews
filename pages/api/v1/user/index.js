605571dca1cdbcf04824e8f14e8347db08013e36
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2026-01-29 20:53:40 +0000

    feat: now the session expires_at is updated by a HTTP request from api/v1/user

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
-
+import session from "models/session.js";
+import user from "models/user";
 const router = createRouter();
 
 router.get(getHandler);
@@ -8,5 +9,14 @@ router.get(getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  return response.status(200).json({});
+  const sessionToken = request.cookies.session_id;
+
+  const sessionObject = await session.findOneValidByToken(sessionToken);
+  const renewedSession = await session.renew(sessionObject.id);
+
+  controller.setSessionCookie(renewedSession.token, response);
+
+  const userFound = await user.findOneById(sessionObject.user_id);
+
+  return response.status(200).json(userFound);
 }
