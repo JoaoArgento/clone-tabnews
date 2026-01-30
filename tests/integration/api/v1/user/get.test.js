8be8ec88f46593c189d1ad5976c90f341e977353
João Pedro <109301783+JoaoArgento@users.noreply.github.com>
2026-01-30 11:33:19 +0000

    feat: configures the endpoint to not use cache

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -27,6 +27,11 @@ describe("GET in api/v1/user", () => {
       expect(response.status).toBe(200);
 
       const responseBody = await response.json();
+      const cacheControl = response.headers.get("Cache-Control");
+
+      expect(cacheControl).toBe(
+        "no-store, no-cache, max-age=0, must-revalidate",
+      );
 
       expect(responseBody).toEqual({
         id: newUser.id,
